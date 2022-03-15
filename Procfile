36a1599ef0abb216b9e0dca33e1848aa4118c16b
achanya <achanyaa4@gmail.com>
2022-03-15 13:31:50 +0530

    commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn institution.wsgi --log-file -
---
+++ institution/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('login/',views.home,name='login'),
+    path('registration/',views.registration,name='registration'),
+]
+
